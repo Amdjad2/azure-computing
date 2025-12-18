9f81f766e5bbc25d7ee0e7ef97268366cc0a4117
Amdjad2 <amdjad.s@hotmail.fr>
2025-12-18 17:41:19 +0000

    Création Azure account Storage via Terrafom

--- infra/main.tf
+++ infra/main.tf
@@ -22,7 +22,28 @@ resource "azurerm_linux_web_app" "webapp" {
   resource_group_name = azurerm_resource_group.rg.name
   service_plan_id     = azurerm_service_plan.app_plan.id
 
+  identity {
+    type = "SystemAssigned"
+  }
+
   site_config {
     always_on = true
   }
 }
+
+resource "azurerm_storage_account" "storage" {
+  name                     = replace("${var.project_name}${var.environment}", "-", "")
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  allow_nested_items_to_be_public = false
+}
+
+resource "azurerm_storage_table" "messages" {
+  name                 = "messages"
+  storage_account_name = azurerm_storage_account.storage.name
+}
+
+
