a2d7390c8ee1c90641ea6e6519a7fa934d97d27c
Amdjad2 <amdjad.s@hotmail.fr>
2025-12-20 11:30:14 +0000

    Creation Keyvault et compte de stockage

--- infra/main.tf
+++ infra/main.tf
@@ -79,9 +79,38 @@ resource "azurerm_key_vault" "kv" {
   resource_group_name         = azurerm_resource_group.rg.name
   tenant_id                   = data.azurerm_client_config.current.tenant_id
   sku_name                    = "standard"
+  enable_rbac_authorization   = true
   purge_protection_enabled    = true
   soft_delete_retention_days  = 7
   public_network_access_enabled = true
 }
 
+
+	### Ajouter un secret
+resource "azurerm_key_vault_secret" "storage_table_name" {
+  name         = "STORAGE-TABLE-NAME"
+  value        = azurerm_storage_table.messages.name
+  key_vault_id = azurerm_key_vault.kv.id
+
+  depends_on = [
+    azurerm_role_assignment.kv_admin_for_terraform
+  ]
+}
+
+	### Accorder un accès temporaire a mon identité
+resource "azurerm_role_assignment" "kv_admin_for_terraform" {
+  scope                = azurerm_key_vault.kv.id
+  role_definition_name = "Key Vault Secrets Officer"
+  principal_id         = data.azurerm_client_config.current.object_id
+}
+
+
+	### Output
+output "storage_account_name" {
+  value = azurerm_storage_account.storage.name
+}
+
+output "key_vault_name" {
+  value = azurerm_key_vault.kv.name
+}
 
